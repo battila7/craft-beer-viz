562686be3f31bcd5828316b0691da5fdf82ea7a2
Attila Bagossy <bagossyattila@outlook.com>
2018-12-03 21:41:46 +0100

    Added the geocoding path and fixed the brewery input path

--- dataset/processor/paths.js
+++ dataset/processor/paths.js
@@ -6,7 +6,8 @@ const inOutputDirectory = (...fragments) => path.join(outputDirectory, ...fragme
 
 module.exports = {
     originalBeers: path.join(__dirname, '..', 'original', 'beers.csv'),
-    originalBreweries: path.join(__dirname, '..', 'original', 'beers.csv'),
+    originalBreweries: path.join(__dirname, '..', 'original', 'breweries.csv'),
 
-    toJson: inOutputDirectory('01-to-json.json')
+    toJson: inOutputDirectory('01-to-json.json'),
+    geocoder: inOutputDirectory('02-geocoder.json')
 };
