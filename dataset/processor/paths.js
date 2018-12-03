b142d38e6cebc8c6f96c5e28d535be2c362f4833
Attila Bagossy <bagossyattila@outlook.com>
2018-12-04 00:04:20 +0100

    Added nationality and type map

--- dataset/processor/paths.js
+++ dataset/processor/paths.js
@@ -1,14 +1,20 @@
 const path = require('path');
 
 const outputDirectory = path.join(__dirname, '..', 'processed');
+const originalDirectory = path.join(__dirname, '..', 'original');
 
 const inOutputDirectory = (...fragments) => path.join(outputDirectory, ...fragments)
+const inOriginalDirectory = (...fragments) => paths.join(originalDirectory, ...fragments);
 
 module.exports = {
-    originalBeers: path.join(__dirname, '..', 'original', 'beers.csv'),
-    originalBreweries: path.join(__dirname, '..', 'original', 'breweries.csv'),
+    originalBeers: inOriginalDirectory('beers.csv'),
+    originalBreweries: inOriginalDirectory('breweries.csv'),
+
+    nationalities: inOriginalDirectory('nationality-map.json'),
+    types: inOriginalDirectory('type-map.json'),
 
     toJson: inOutputDirectory('01-to-json.json'),
     geocoder: inOutputDirectory('02-geocoder.json'),
-    beerIntoBrewery: inOutputDirectory('03-beer-into-brewery.json')
+    beerIntoBrewery: inOutputDirectory('03-beer-into-brewery.json'),
+    nationalityAndType: inOutputDirectory('04-nationality-and-tyoe.json')
 };
